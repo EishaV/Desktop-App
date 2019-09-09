69d792dd23825e92d2837e0deba6549c9749444a
EishaV <eisha@gmx.de>
2019-09-09 15:13:55 +0200

    Update AssemblyInfo.cs

--- MqttJson/AssemblyInfo.cs
+++ MqttJson/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("LandroidS")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("MqttJson")]
+[assembly: AssemblyDescription("Plugin Interface")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Landroid S")]
-[assembly: AssemblyCopyright("Copyright © EishaV 2017")]
+[assembly: AssemblyProduct("Desktop App")]
+[assembly: AssemblyCopyright("Copyright © EishaV 2017-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.14.*")]
-[assembly: AssemblyFileVersion("0.0.14")]
+[assembly: AssemblyVersion("0.0.24.*")]
+[assembly: AssemblyFileVersion("0.0.24")]
